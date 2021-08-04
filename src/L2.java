214fd966945e21b51dbde31a39d7f37c2b1806f2
priceoli000 <priceoli000@warren.k12.in.us>
2021-08-04 11:51:32 -0400

    Completed L1 and L2 Notes

--- src/L2.java
+++ src/L2.java
@@ -1,4 +1,18 @@
 public class L2 {
 
     //add the main method header
+    public static void main(String[] args) {
+       String firstName = "George";
+        int age = 16;
+        System.out.println(firstName);
+        System.out.println(age);
+
+        String clientName = "Oliver";
+        String cardNumber = "1234 5678 9101 1213";
+        int cvvNumber = 123;
+      System.out.println(clientName);
+      System.out.println(cardNumber);
+      System.out.println(cvvNumber);
+
+    }
 }
