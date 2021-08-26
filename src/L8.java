f9864eb92be964a679fa16ba4c1add676f0f72c5
priceoli000 <priceoli000@warren.k12.in.us>
2021-08-26 12:21:54 -0400

    Completed L8 & L9 Notes.

--- src/L8.java
+++ src/L8.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class L8 {
 
     public static void main(String[] args) {
-        String name;
+
+        Scanner read = new Scanner(System.in);
+        /*String name;
         int hours;
         double payRate;
         double grossPay;
@@ -27,6 +29,34 @@ public class L8 {
         grossPay = hours * payRate;
 
         System.out.println("Hello " + name);
-        System.out.println("Your gross pay is $" + grossPay);
+        System.out.println("Your gross pay is $" + grossPay);*/
+
+        // Bugaboo #1 - Hanging carriage return
+      /*  int age;
+        String name;
+
+        System.out.println("What is your age?");
+        age = read.nextInt();
+
+        System.out.println("What is your name?");
+        name = read.next();
+
+        System.out.println("What is your graduation year?");
+        int year = read.nextInt();
+
+       read.nextLine(); //Eats up hanging carriage returns
+
+        System.out.println("hello " + name + ",you are " + age + "years old!");*/
+
+        // Bugaboo #2 - Tokens
+        String firstName;
+        String lastName;
+
+        System.out.println("What is your full name with age?");
+        firstName = read.next();
+        lastName = read.next();
+        String age = read.nextLine();
+
+        System.out.println("Hello, " + firstName + " " + lastName + " You are" + age + " years old.");
     }
 }
